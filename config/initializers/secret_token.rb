e72e2bdbe393554d17a1c9c4004b7f19314bd804
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-04-26 10:20:01 -0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseConfirmableMail::Application.config.secret_token = 'fafb049399183910e89f926907381ef9533a1565d08705af8c509d23ca98223315053d6287a95c16c2eeade2c76c9d7ea92b7d1421b691a661a6b3264eed9eac'
