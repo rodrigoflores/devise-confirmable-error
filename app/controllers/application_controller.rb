27249dab94a4a20d420d83d03e569e826bd4f831
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-04-26 10:30:25 -0300

    Devise test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_filter :mailer_set_url_options
+
   protect_from_forgery
+
+  def mailer_set_url_options
+    ActionMailer::Base.default_url_options[:host] = request.host_with_port
+  end
 end
